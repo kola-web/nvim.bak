87bec8e5d153da9bd7fd74848e56f284f83468f6
kola <l15567540858@gmail.com>
2022-03-13 10:07:31 +0800

    theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,11 +5,13 @@
 -- colorscheme gruvbox
 -- colorscheme onedark
 
-vim.cmd [[
-try
-  colorscheme tokyonight
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+-- vim.cmd [[
+-- try
+--   colorscheme tokyonight
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]]
+
+require('github-theme').setup()
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,7 @@ return packer.startup(function(use)
   use "morhetz/gruvbox"
   use "joshdick/onedark.vim"
   use "navarasu/onedark.nvim"
+  use "projekt0n/github-nvim-theme"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
